9a7a9f29d532ebcf255d6c608b00483e3c3510ff
Jaehun Kim <wogns3623@gmail.com>
2025-09-01 19:47:16 +0900

    chmod

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -4,9 +4,9 @@ set -e
 set -x
 
 cd backend
-python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
+uv run python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
 cd ..
 mv openapi.json frontend/
 cd frontend
-npm run generate-client
-npx biome format --write ./src/client
+pnpm run generate-client
+pnpx biome format --write ./src/client
