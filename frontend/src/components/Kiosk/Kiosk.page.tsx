42b639ee0f564553a347e9daf50c8ec498a99243
Jaehun Kim <wogns3623@gmail.com>
2025-09-15 22:46:21 +0900

    Remove unused imports in KioskPageInner component

--- frontend/src/components/Kiosk/Kiosk.page.tsx
+++ frontend/src/components/Kiosk/Kiosk.page.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { useState } from "react";
 
 import { useMenusSuspense } from "@/hooks/useMenu";
 import type { CartItem } from "@/types/cart";
