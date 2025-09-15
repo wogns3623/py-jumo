c4ac9e1e6766dda7b2156946765708078d1eb72a
kim_mon_ey <113882868+kimmoney@users.noreply.github.com>
2025-09-15 18:34:08 +0900

    fix copy to clipboard

--- frontend/src/utils/payment.ts
+++ frontend/src/utils/payment.ts
@@ -8,16 +8,14 @@ export function getPaymentUrl(order: OrderWithPaymentInfo) {
 export function requestPayment(order: OrderWithPaymentInfo) {
   const paymentUrl = getPaymentUrl(order);
 
-  setTimeout(() => {
-    document.execCommand(
-      "copy",
-      false,
-      `${order.payment_info.bank_name} ${order.payment_info.bank_account_no} ${order.final_price}원`
-    );
-    toast.success(
-      "계좌번호 복사 완료! 이체 앱으로 이동하여 결제를 완료해주세요."
-    );
-  }, 100);
+  document.execCommand(
+    "copy",
+    false,
+    `${order.payment_info.bank_name} ${order.payment_info.bank_account_no} ${order.final_price}원`
+  );
+  toast.success(
+    "계좌번호 복사 완료! 이체 앱으로 이동하여 결제를 완료해주세요."
+  );
 
   window.location.href = paymentUrl;
 }
