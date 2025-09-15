a74d916314fdc092144b3521687780cf22a70c63
kim_mon_ey <113882868+kimmoney@users.noreply.github.com>
2025-09-15 18:40:41 +0900

    클립보드 복사 수정

--- frontend/src/utils/payment.ts
+++ frontend/src/utils/payment.ts
@@ -8,11 +8,13 @@ export function getPaymentUrl(order: OrderWithPaymentInfo) {
 export function requestPayment(order: OrderWithPaymentInfo) {
   const paymentUrl = getPaymentUrl(order);
 
+  const text = `${order.payment_info.bank_name} ${order.payment_info.bank_account_no} ${order.final_price}원`
   document.execCommand(
     "copy",
     false,
-    `${order.payment_info.bank_name} ${order.payment_info.bank_account_no} ${order.final_price}원`
+    text
   );
+  navigator.clipboard.writeText(text)
   toast.success(
     "계좌번호 복사 완료! 이체 앱으로 이동하여 결제를 완료해주세요."
   );
