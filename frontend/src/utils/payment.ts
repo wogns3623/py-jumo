425ea2840035f2b0e9a593fd078d781a5301eb7e
Jaehun Kim <wogns3623@gmail.com>
2025-09-15 20:04:37 +0900

    계좌 복사 진심수정

--- frontend/src/utils/payment.ts
+++ frontend/src/utils/payment.ts
@@ -8,16 +8,42 @@ export function getPaymentUrl(order: OrderWithPaymentInfo) {
 export function requestPayment(order: OrderWithPaymentInfo) {
   const paymentUrl = getPaymentUrl(order);
 
-  const text = `${order.payment_info.bank_name} ${order.payment_info.bank_account_no} ${order.final_price}원`
-  document.execCommand(
-    "copy",
-    false,
-    text
-  );
-  navigator.clipboard.writeText(text)
+  const text = `${order.payment_info.bank_name} ${order.payment_info.bank_account_no} ${order.final_price}원`;
+
+  copyToClipboard(text);
+  navigator.clipboard.writeText(text);
+  document.execCommand("copy", false, text);
   toast.success(
-    "계좌번호 복사 완료! 이체 앱으로 이동하여 결제를 완료해주세요."
+    "토스가 없으신가요? 대신 계좌번호가 복사되었으니 원하시는 이체 앱으로 이동하여 결제를 완료해주세요!",
+    { duration: 10000 }
   );
 
   window.location.href = paymentUrl;
+}
+
+function isOS() {
+  return navigator.userAgent.match(/ipad|iphone/i);
+}
+
+function copyToClipboard(text: string) {
+  const textArea = document.createElement("textArea") as HTMLTextAreaElement;
+  textArea.value = text;
+  document.body.appendChild(textArea);
+
+  var range;
+
+  if (isOS()) {
+    range = document.createRange();
+    range.selectNodeContents(textArea);
+    const selection = window.getSelection();
+    if (!selection) return;
+    selection.removeAllRanges();
+    selection.addRange(range);
+    textArea.setSelectionRange(0, 999999);
+  } else {
+    textArea.select();
+  }
+
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
 }
